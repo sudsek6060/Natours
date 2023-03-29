324951523d3949f1ea586ed1cc1c5d434a3b779d
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-29 21:45:04 +0530

    pathch and delete

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
 
     const id = req.params.id * 1;
-        const tour = tours.find(el => el.id === id)
+    const tour = tours.find(el => el.id === id)
 
     // if(id > tours.length) {
-        if(!tour){
+    if (!tour) {
         return res.status(404).json({
             status: 'failed',
             message: 'Invalid id'
@@ -42,24 +42,54 @@ app.get('/api/v1/tours/:id', (req, res) => {
             tour
         }
     })
-})
+});
 
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
     const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
+    const newTour = Object.assign({ id: newId }, req.body);
 
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: 'success',
-            data : {
+            data: {
                 tours: newTour
             }
         })
     })
+});
 
-   
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length) {
+        
+            return res.status(404).json({
+                status: 'failed',
+                message: 'Invalid id'
+            })
+        }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here.....>'
+        }
+    })
+})
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length) {
+        
+            return res.status(404).json({
+                status: 'failed',
+                message: 'Invalid id'
+            })
+        }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
 })
 const port = 3000;
 app.listen(port, () => {
