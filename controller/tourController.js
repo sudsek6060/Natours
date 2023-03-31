9ad5b8ed67c109667e0d1cb5c2a8376e0aa3c9f9
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-31 06:49:27 +0530

    middleware in post

--- controller/tourController.js
+++ controller/tourController.js
@@ -14,7 +14,17 @@ exports.checkId = (req, res, next, val) => {
         })
     }
     next();
-}
+};
+
+exports.checkBody = (req, res, next) =>{
+    if(!req.body.name || !req.body.price){
+        return res.status(400).json({
+            status: 'failed',
+            message: 'Missing name or price'
+        })
+    }
+    next();
+};
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
