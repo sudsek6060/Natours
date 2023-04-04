c0618a6943b6ad409fd8689bd44b080363294305
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-04 13:26:50 +0530

    eslint plugins

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const app = require('./app')
+const dotenv = require('dotenv');
+const app = require('./app');
 
-const port = 3000;
+dotenv.config({ path: './config.env' });
+
+
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App is running on por ${port}`);
-})
+  console.log(`App is running on por ${port}`);
+});
