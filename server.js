21aad01207826cebf56d51e35672a42cc433bf60
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-05 00:01:39 +0530

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('DB connected sucessfully'))
 
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App is running on por ${port}`);
+    console.log(`App is running on por ${port}`);
 });
