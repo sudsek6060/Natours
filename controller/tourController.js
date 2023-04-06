ae7c4b25d04e054d1d6398a72bc7575b6592ba64
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-06 16:16:15 +0530

    complete schema

--- controller/tourController.js
+++ controller/tourController.js
@@ -58,7 +58,7 @@ exports.createTour = async (req, res) => {
     } catch (error) {
         res.status(400).json({
             status: 'failed',
-            message: 'Invalid data set'
+            message: error
         })
     }
 };
