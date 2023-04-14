e0732499b233a47f1e58cea0f516d423c978973b
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-14 19:45:56 +0530

    handling mongoose database errors

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,16 @@
+const AppError = require('../Utils/appError');
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`
+    return new AppError(message, 400)
+}
+
+const handleDuplicateErrorDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}, please use another value!`
+    return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -31,6 +44,10 @@ module.exports = (err, req, res, next) => {
     if(process.env.NODE_ENV === 'development'){
         sendErrorDev(err, res)
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res)
+        let error = {...err}
+        if(error.name === 'CastError') error = handleCastErrorDB(error)
+        if(error.code === 11000) error = handleDuplicateErrorDB(error)
+
+        sendErrorProd(error, res)
     }
 }
