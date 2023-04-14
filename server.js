ff7c857a3d583aa2677e5ba56096141e331ae5d7
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-14 20:17:33 +0530

    unhandled rejections

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose.connect(DB, {
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App is running on por ${port}`);
 });
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLER REJECTION!  Shutting down....');
+    server.close( () => {
+            process.exit(1);
+    })
+})
