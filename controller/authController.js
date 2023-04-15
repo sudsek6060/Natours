8b2afe6b5ceb168d8f404f7d06213de6a137b878
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-15 14:24:06 +0530

    auth controller

---
+++ controller/authController.js
@@ -0,0 +1,13 @@
+const User = require('../model/userModel');
+const catchAsync = require('../Utils/catchAsync');
+
+exports.signup = catchAsync( async (req, res, next) => {
+    const newUser = await User.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            user: newUser
+        }
+    })
+});
