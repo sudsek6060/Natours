4b2da16c4cf8752ee153a35f8533146fa0864808
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-17 13:12:54 +0530

    setting new password

--- model/userModel.js
+++ model/userModel.js
@@ -51,6 +51,13 @@ userSchema.pre('save', async function(next) {
     next()
 });
 
+userSchema.pre('save', function(next) {
+    if(!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+})
+
 userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
     return await bcrypt.compare(candidatePassword, userPassword)
 };
@@ -63,18 +70,20 @@ userSchema.methods.changePasswordAfter = function(JWTTimeStamp){
     return false
 }
 
-userSchema.methods.createPasswordResetToken = function(){
+userSchema.methods.createPasswordResetToken = function() {
     const resetToken = crypto.randomBytes(32).toString('hex');
-
+  
     this.passwordResetToken = crypto
-    .createHash('sha256')
-    .update(resetToken)
-    .digest('hex')
-
+      .createHash('sha256')
+      .update(resetToken)
+      .digest('hex');
+  
+    // console.log({ resetToken }, this.passwordResetToken);
+  
     this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
+  
     return resetToken;
-};
+  };
 
 const User = mongoose.model('User', userSchema);
 
