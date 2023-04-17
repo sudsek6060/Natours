61bdac7a50502711e18ec3571519cb3c864ef798
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-17 15:54:14 +0530

    updating user data

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,14 @@
 const User = require('../model/userModel');
-const catchAsync = require('../Utils/catchAsync')
+const catchAsync = require('../Utils/catchAsync');
+const AppError = require('../Utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el]
+    });
+    return newObj;
+}
 
 exports.getAllUsers = catchAsync(async (req, res) => {
     const users = await User.find();
@@ -18,6 +27,27 @@ exports.createUser = (req, res) => {
         message: 'This outer is not defined '
     })
 }
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // 1. Create errpr if user posts password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This route is not for password update, please use /updateMypassword', 400))
+    }
+
+    // 1.Filtered out unwanted fields names that are no allowed
+    const filterBody = filterObj(req.body, 'name', 'email');
+    // 2. Update user document
+    const updateUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+        new: true,
+        runValidators: true
+    });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updateUser
+        }
+    })
+})
 exports.getUser = (req, res) => {
     res.status(500).json({
         success: 'Error',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,7 @@ router.post('/forgetPassword', authController.forgotPassword)
 router.patch('/resetPassword/:token', authController.resetPassword)
 
 router.patch('/updateMypassword', authController.protect, authController.updatePassword)
+router.patch('/updateMe', authController.protect, userController.updateMe)
 
 router
     .route('/')
