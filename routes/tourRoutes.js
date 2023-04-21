8c82291a0225e1bab817d6039c612c727e60ae0f
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-21 12:28:29 +0530

    nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const tourController = require('../controller/tourController');
 const authController = require('../controller/authController')
+const reviewRouter = require('../routes/reviewRoutes')
 
 const router = express.Router();
 
 // router.param('id', tourController.checkId)
+
+router.use('/:tourId/reviews', reviewRouter)
 
 router
     .route('/top-5-cheap')
@@ -25,6 +28,8 @@ router
     .delete(authController.protect,
          authController.restrictTo('admin', 'lead-guide'),
           tourController.deleteTour)
-
+//  router
+//     .route('/:tourId/reviews')
+//     .post(authController.protect, authController.restrictTo('user'), reviewController.createReview)
 
 module.exports = router;
