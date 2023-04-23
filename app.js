2834fee546b9f4eb738d107f05af039acfa3feff
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-23 05:58:52 +0530

    setting up pug

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const path = require('path')
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,9 +14,15 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
-const app = express()
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
 
 // Global Middleware
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // set security HTTP headers
 app.use(helmet())
 
@@ -52,8 +59,6 @@ app.use(hpp({
     ]
 }));
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
 
 // Test middleware
 app.use((req, res, next) => {
@@ -77,6 +82,9 @@ app.use((req, res, next) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+app.get('/', (req, res) => {
+    res.status(200).render('base')
+})
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
