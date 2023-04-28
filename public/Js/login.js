8d480d15abe18ade3e25bd819f6b7b99a08370f0
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-04-28 15:05:23 +0530

    logout user

--- public/Js/login.js
+++ public/Js/login.js
@@ -15,7 +15,9 @@ const showAlert = (type, msg) => {
     document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
     window.setTimeout(hideAlert, 5000);
   };
-  
+
+  const loginForm = document.querySelector('.form--login');
+  const logOutBtn = document.querySelector('.nav__el--logout');
 
 const login = async (email, password) => {
     try {
@@ -40,10 +42,25 @@ const login = async (email, password) => {
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.form').addEventListener('submit', function(e) {
+    loginForm.addEventListener('submit', function(e) {
         e.preventDefault();
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         login(email, password);
       });
 });
+
+const logout = async () => {
+    try {
+      const res = await axios({
+        method: 'GET',
+        url: 'http://127.0.0.1:3000/api/v1/users/logout'
+      });
+      if ((res.data.status = 'success')) location.reload(true);
+    } catch (err) {
+      console.log(err.response);
+      showAlert('error', 'Error logging out! Try again.');
+    }
+  };
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
